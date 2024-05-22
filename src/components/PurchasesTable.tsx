4b03cb4ffe55c6396abd6c407cab667d21494222
carrichi <roberto@carrichi.com>
2024-05-22 02:36:19 -0600

    feat: :zap: New purchase table with backend connection!

---
+++ src/components/PurchasesTable.tsx
@@ -0,0 +1,143 @@
+import { FaceSmileIcon, HandRaisedIcon, HandThumbDownIcon, HomeIcon, QuestionMarkCircleIcon } from "@heroicons/react/24/outline"
+import { SetStateAction, useEffect, useState } from "react"
+
+const host = import.meta.env.VITE_BACKEND_HOST
+const USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
+const formatDateToISO = (timestamp : string) => {
+  const months : {[key: number]: string} = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dic',
+  }
+  const date = new Date(timestamp)
+  return date.getDate() + '/' + months[date.getMonth()] + '/' + date.getFullYear()
+}
+const category_icons = {
+  'HOME': HomeIcon,
+  'PERSONAL': FaceSmileIcon,
+  'EMERGENCY': HandRaisedIcon,
+  'SKIPPEABLE': HandThumbDownIcon,
+  'OTHER': QuestionMarkCircleIcon
+}
+
+export default function PurchasesTable() {
+  const [purchases, SetPurchases] = useState<any[]>([])
+
+  const getPurchases = async () => {
+    const purchases = await fetch(`${host}/purchases`).then(res => res.json())
+    const display_data: SetStateAction<any[]> = []
+    purchases.map((purchase: { category: any }) => display_data.push({
+      ...purchase,
+      category_icon: category_icons[purchase.category ? purchase.category : 'OTHER'],
+    }))
+    console.log(display_data)
+    SetPurchases(display_data)
+  }
+
+  useEffect(() => { getPurchases() }, [])
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Purchases</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all of your purchases in your account including their name, amount and status.
+          </p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add purchase
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    Name
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Amount
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Status
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Applied At
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Deadline
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {purchases.map((purchase) => (
+                  <tr key={purchase.id}>
+                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                      <div className="flex items-center">
+                        <div className="h-11 w-11 flex-shrink-0">
+                          {/* <img className="h-11 w-11 rounded-full" src={purchase.id} alt="" /> */}
+                          <purchase.category_icon />
+                        </div>
+                        <div className="ml-4">
+                          <div className="font-medium text-gray-900">{purchase.concept}</div>
+                          <div className="mt-1 text-gray-500">{purchase.notes}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <div className="text-gray-900">{USDollar.format(purchase.amount)}</div>
+                      <div className="mt-1 text-gray-500">{purchase.payment_method}</div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        Active
+                      </span>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{
+                      purchase.applied_at
+                      ? formatDateToISO(purchase.applied_at)
+                      : ''
+                    }</td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{
+                      purchase.deadline
+                      ? formatDateToISO(purchase.deadline)
+                      : ''
+                    }</td>
+                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
